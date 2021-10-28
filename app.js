30c9744d4280441ce338a036267a46c83435d819
kevin <kevinarmando.odarsantiago@halliburton.com>
2021-10-28 02:37:34 -0300

    port changed

--- app.js
+++ app.js
@@ -13,7 +13,9 @@ const app = express();
 
 const connectDB = require('./db/connect');
 
-const { MONGO_URI, DB_USER, DB_PASS, DATABASE_NAME, port = 3000 } = process.env;
+const { MONGO_URI, DB_USER, DB_PASS, DATABASE_NAME } = process.env;
+
+const port = 3001;
 
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
@@ -47,6 +49,7 @@ const start = async () => {
     await connectDB(
       `mongodb+srv://${DB_USER}:${DB_PASS}@${MONGO_URI}/${DATABASE_NAME}?authSource=admin&replicaSet=atlas-wofvy2-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true`
     );
+    // eslint-disable-next-line no-console
     app.listen(port, console.log(`Server is listening on port ${port}...`));
   } catch (error) {
     // eslint-disable-next-line no-console
